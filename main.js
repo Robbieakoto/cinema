c9afb2443b7d229ecb16a75c5bec690085748501
Robbie <akotoro22@gmail.com>
2018-08-01 13:29:24 +0000

    Made changes to the genres

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const movieList = document.getElementById('row');
-const genreList = document.getElementById('grid');
 
 let listOfMovies = '';
-let listOfGenres = '';
 
 fetch('https://silverbird-scraper.glitch.me/movies')
     .then(response => {
@@ -31,41 +29,38 @@ fetch('https://silverbird-scraper.glitch.me/movies')
     })
 
 
-let genre = document.getElementById('genre');
-const optionChange = genres => {
-    for (let gen in genres) {
-        let genre = genres[gen];
-
-        if (genre) {
-            let option = document.createElement('option');
-            option.text = `${genre.genresIndex} (${genre.index})`;
-            from.add(option);
-        }
-    }
-
-}
-
 let changeOption = () => {
+    let selection = document.getElementById('genre').value;
 
     fetch('https://silverbird-scraper.glitch.me/movies')
         .then(response => {
             response.json().then((result) => {
+
+                const genreList = document.getElementById('grid');
+                let listOfGenres = [];
+
                 result.movies.map(({ thumbnail, title, url, genres }) => {
+                    genrelength = genres.length;
+                    for (i = 0; i <= genrelength; i++) {
 
-                    optionChange(response.results)
+                        if (selection == genres[i]) {
+                            listOfGenres = listOfGenres + `
+                                <div class="col-lg-4" style="padding:10px">
+                                    <div class="card" style="padding:10px">
+                                        <img class="card-img-top" src="${thumbnail}">
+                                        <div class="card-body">
+                                        <h5 class="card-title" style="color:#2A96C8">${title}</h5>
+                                        <p> <span style="color:#B34E4B"> Genre </span>: ${genres}</p>
+                                        <a class="btn btn" href ="${url}" style=" background-color:#2A96C8; color:#fff">See more</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            `
 
-                    listOfGenres = listOfGenres + `
-                    <div class="col-lg-4" style="padding:10px">
-                        <div class="card" style="padding:10px">
-                            <img class="card-img-top" src="${thumbnail}">
-                            <div class="card-body">
-                            <h5 class="card-title" style="color:#2A96C8">${title}</h5>
-                            <p> <span style="color:#B34E4B"> Genre </span>: ${genres}</p>
-                            <a class="btn btn" href ="${url}" style=" background-color:#2A96C8; color:#fff">See more</a>
-                            </div>
-                        </div>
-                    </div>
-                `
+                        }
+
+                    }
+
                 })
                 genreList.innerHTML = listOfGenres;
             })
